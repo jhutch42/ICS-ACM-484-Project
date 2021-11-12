27af5d0a02891962eb483667003fbaf9a280bc64
James <jhutch42@hawaii.edu>
2021-11-11 17:32:37 -1000

    removed files to speed up commit

--- src/server.js
+++ src/server.js
@@ -35,25 +35,25 @@ async function processLineByLine(callback) {
 // processLineByLine(parseCSV);
 //createOpeningsJSONFile(checkForCompletion);
 
-function parseCSV(dataArray) {
-    let headerArray = dataArray[0].split(',');
-    const breaks = [0, 40000, 80000, 120000, 160000, 200000, 240000, 280000, 320000, 360000, 400000, 440000, 480000, 520000, 560000, 600000];
-    for (let k = 0; k < breaks.length - 1; k++) {
-        const jsonData = [];
-        for (let i = breaks[k]; i < breaks[k + 1]; i++) {
-            let jsonObject = {};
-            const lineArray = dataArray[i].split(',');
-            for (let j = 0; j < headerArray.length; j++) {
-                jsonObject[headerArray[j]] = lineArray[j];
-            }
-            if (jsonObject.Termination !== 'Abandoned' && jsonObject.Termination !== 'Rules infraction' && jsonObject.Termination !== 'Time forfeit') jsonData.push(jsonObject);
-            if (i % 50000 === 0) console.log(i);
-        }
-        fs.writeFile('chessDataFiles/ratedClassicalGame_' + k + '.json', JSON.stringify(jsonData), (error, result) => {
-            if (error) console.log(error);
-        });
-    }
-}
+// function parseCSV(dataArray) {
+//     let headerArray = dataArray[0].split(',');
+//     const breaks = [0, 40000, 80000, 120000, 160000, 200000, 240000, 280000, 320000, 360000, 400000, 440000, 480000, 520000, 560000, 600000];
+//     for (let k = 0; k < breaks.length - 1; k++) {
+//         const jsonData = [];
+//         for (let i = breaks[k]; i < breaks[k + 1]; i++) {
+//             let jsonObject = {};
+//             const lineArray = dataArray[i].split(',');
+//             for (let j = 0; j < headerArray.length; j++) {
+//                 jsonObject[headerArray[j]] = lineArray[j];
+//             }
+//             if (jsonObject.Termination !== 'Abandoned' && jsonObject.Termination !== 'Rules infraction' && jsonObject.Termination !== 'Time forfeit') jsonData.push(jsonObject);
+//             if (i % 50000 === 0) console.log(i);
+//         }
+//         fs.writeFile('chessDataFiles/ratedClassicalGame_' + k + '.json', JSON.stringify(jsonData), (error, result) => {
+//             if (error) console.log(error);
+//         });
+//     }
+// }
 // const openings = [];
 // function createOpeningsJSONFile(callback) {
 
