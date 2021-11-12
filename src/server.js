2e06e95201f4eeb5f736336788b899b434d9d1a4
jhutch42 <jhutch42@hawaii.edu>
2021-11-11 19:01:56 -1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // Define Express App
 const app = express();
-app.use(express.static('./public'));
+app.use(express.static('./public')); 
 
 const PORT = process.env.PORT || 4040;
 
